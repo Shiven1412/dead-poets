c365b4dcd387d65e47796e4e3e4374060d65cb7c
shivendra <shivendratripathi2876@gmail.com>
2025-04-13 19:50:40 +0530

    fixed backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 connectDB();
 
 app.use(cors({
-  origin: 'https://dead-poets-nine.vercel.app',
+  origin: 'https://dead-poets-society.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
